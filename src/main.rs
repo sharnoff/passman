787ea0490c45abebd8821656e2eba2f567a6d403
sharnoff <github@max.sharnoff.org>
2020-08-26 08:17:17 +0100

    Add re-draw on window resize

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use signal_hook::{iterator::Signals, SIGWINCH};
 use std::io::{self, Stdout};
+use std::sync::mpsc::{channel, Receiver};
+use std::thread;
+use termion::event::Event;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
 use tui::backend::TermionBackend;
@@ -33,14 +37,57 @@ fn main() -> Result<(), io::Error> {
     ui::draw(&mut terminal, &app)?;
 
     // And then we simply run all while we have key inputs
-    for res in io::stdin().events() {
-        if !app.handle(res?) {
-            terminal.clear()?;
-            break;
+    for item in events()? {
+        if let Some(event) = item {
+            if !app.handle(event?) {
+                terminal.clear()?;
+                break;
+            }
         }
 
         ui::draw(&mut terminal, &app)?;
     }
 
     Ok(())
+}
+
+// Produces an iterator over all of the terminal events
+//
+// This is essentially a wrapper around stdin().events() to additionally give resizes as an event,
+// so we return None if it was a resize.
+fn events() -> io::Result<impl Iterator<Item = Option<io::Result<Event>>>> {
+    // In order to do this properly, we need multiple threads to handle
+    struct Iter {
+        rx: Receiver<Option<io::Result<Event>>>,
+    }
+
+    impl Iterator for Iter {
+        type Item = Option<io::Result<Event>>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.rx.recv().ok()
+        }
+    }
+
+    let (tx, rx) = channel();
+    let iter = Iter { rx };
+
+    // We'll spawn two threads to handle sending into the channel. The first will produce events
+    // from resizes:
+    let signals = Signals::new(&[SIGWINCH])?;
+    let tx_cloned = tx.clone();
+    thread::spawn(move || {
+        for _ in &signals {
+            tx_cloned.send(None).unwrap();
+        }
+    });
+
+    // While the second will simply forward on the events from stdin, wrapping them with `Some`
+    thread::spawn(move || {
+        for res in io::stdin().events() {
+            tx.send(Some(res)).unwrap();
+        }
+    });
+
+    Ok(iter)
 }
